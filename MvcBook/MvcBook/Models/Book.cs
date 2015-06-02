c0965fb93f2cf5e3ba4120742ea86579e7698576
merveecetin <merve.ctnn93@gmail.com>
2015-06-02 22:07:28 +0300

    blog

---
+++ MvcBook/MvcBook/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcBook.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+       
+        public string Genre { get; set; }
+
+       
+
+        public decimal Price { get; set; }
+        
+       
+
+    }
+    
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ MvcBook/MvcBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcBook.Startup))]
+namespace MvcBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
